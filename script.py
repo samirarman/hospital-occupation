7d1ea50ef2e3d51d9e935026222cf4a823fbc724
samir <samir.arman@gmail.com>
2021-01-08 19:45:46 -0300

    Fix #1

--- script.py
+++ script.py
@@ -12,14 +12,25 @@ for line in lines:
 
 # Reads the json file
 # into a DataFrame
-df = pd.DataFrame()
+new_data = pd.DataFrame()
 for line in data:
-    df = df.append(pd.DataFrame.from_records(pd.DataFrame.from_records(line['hits']['hits'])['_source']))
+    new_data = new_data.append(pd.DataFrame.from_records(pd.DataFrame.from_records(line['hits']['hits'])['_source']))
 
 # Converts date to correct type
-df['dataNotificacaoOcupacao'] = pd.to_datetime(df['dataNotificacaoOcupacao'], infer_datetime_format=True).dt.date
+new_data['dataNotificacaoOcupacao'] = pd.to_datetime(new_data['dataNotificacaoOcupacao'], infer_datetime_format=True).dt.date
+
+
+# Reads the previous data
+# and reshapes it as needed
+prev_data = pd.read_csv("occupation.csv", low_memory = False)
+prev_data = prev_data.drop(prev_data.columns[0], axis = 1)
+prev_data = prev_data[new_data.columns]
+
+# Appends the new data to previous data
+df = prev_data.append(new_data)
 
 # Sorts the dataframe and save a csv image of it
 df = df.sort_values(by = ['estadoSigla', 'municipio', 'nomeCnes', 'dataNotificacaoOcupacao'])
 df = df.drop_duplicates()
+
 df.to_csv("occupation.csv")
